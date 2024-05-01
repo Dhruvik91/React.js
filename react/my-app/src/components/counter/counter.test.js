f83bed557fe057e18e7a76489db182cfd0586ac1
Dhruvik91 <yashgondaliya04@gmail.com>
2024-05-01 18:01:59 +0530

    Learn about keyboard interaction testing

--- react/my-app/src/components/counter/counter.test.js
+++ react/my-app/src/components/counter/counter.test.js
@@ -29,4 +29,36 @@ describe("Counter", () => {
     const textHeader = screen.getByRole("heading");
     expect(textHeader).toHaveTextContent("1");
   });
+
+  test("render 10 on click", async () => {
+    user.setup();
+    render(<Counter />);
+    const amount = screen.getByRole("spinbutton");
+    await user.type(amount, "10");
+    expect(amount).toHaveValue(10);
+    const button = screen.getByRole("button", {
+      name: "Set",
+    });
+    await user.click(button);
+    const heading = screen.getByRole("heading");
+    expect(heading).toHaveTextContent("10");
+  });
+
+  test("change focused on tab", async () => {
+    user.setup();
+    render(<Counter />);
+    const amount = screen.getByRole("spinbutton");
+    const button = screen.getByRole("button", {
+      name: "Increment",
+    });
+    const setButton = screen.getByRole("button", {
+      name: "Set",
+    });
+    await user.tab();
+    expect(button).toHaveFocus();
+    await user.tab();
+    expect(amount).toHaveFocus();
+    await user.tab();
+    expect(setButton).toHaveFocus();
+  });
 });
